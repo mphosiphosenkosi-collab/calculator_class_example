7f097c5ddd2510a8187fbd9e614bd2cc639f7b7c
Siphosenkosi <Siphosenkosi17@outlook.com>
2026-02-04 12:41:06 +0200

    Fix: update calculation controllers fixing name space and renaming erros updating calculation request

--- calculator_class_example/Domain/CalculationRequest.cs
+++ calculator_class_example/Domain/CalculationRequest.cs
@@ -1,6 +1,9 @@
-namespace Calculator_Class_Example.Domain;
-public record CalculationRequest(
-    double left,
-    double right,
-    OperationType Operation
-);
+namespace Calculator_Class_Example.Domain
+{
+    public class CalculationRequest
+    {
+        public double Left { get; set; }
+        public double Right { get; set; }
+        public OperationType Operation { get; set; }
+    }
+}
